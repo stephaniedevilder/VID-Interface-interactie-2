1e4c351104545cfa7c78c849171c3104ebad225c
stephaniedevilder <94384514+stephaniedevilder@users.noreply.github.com>
2022-11-10 13:13:52 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,56 @@
 // JavaScript Document
-console.log("Howdy!");
+
+
+// TOBEY //
+
+// Variabel aanmaken
+var tobeyButton = document.querySelector('li:nth-of-type(1) button');
+var spinWeb = document.querySelector('li:nth-of-type(1) > img');
+var tobeyInfoBlok = document.querySelector('li:nth-of-type(1) section');
+
+// EventListener aanmaken
+tobeyButton.addEventListener('click', showTobeyDetails)
+
+// Functie maken
+function showTobeyDetails(){
+    spinWeb.classList.toggle('webSchiet');
+    
+    setTimeout(() => {
+        tobeyInfoBlok.classList.toggle('tobeyDetailsShow');
+      }, "800")
+}
+
+
+
+
+// GARFIELD //
+
+// Variabel aanmaken
+var garfieldButton = document.querySelector('li:nth-of-type(2) button');
+var garfieldInfoBlok = document.querySelector('li:nth-of-type(2) section')
+
+// EventListener aanmaken
+garfieldButton.addEventListener('click', showGarfieldDetails)
+
+// Functie maken
+function showGarfieldDetails(){
+    garfieldButton.classList.toggle('drop');
+    garfieldInfoBlok.classList.toggle('GarfieldDetailsShow')
+}
+
+
+
+
+// TOM HOLLAND //
+
+// Variabel aanmaken
+var tomButton = document.querySelector('li:nth-of-type(3) button');
+var holoGram = document.querySelector('li:nth-of-type(3) section');
+
+// EventListener aanmaken
+tomButton.addEventListener('click', showTomDetails)
+
+// Functie maken
+function showTomDetails(){
+    holoGram.classList.toggle('holoGramKlaptUit');
+}
